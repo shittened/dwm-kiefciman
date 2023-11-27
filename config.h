501600478b6eb0f78ae376736ea2928633febad2
Kiefciman <kiefciman@gmail.com>
2023-11-27 21:47:33 +0200

    a

--- config.h
+++ config.h
@@ -4,16 +4,17 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;  /* 0 means bottom bar */
-static const int user_bh            = 25;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int topbar             = 1;		/* 0 means bottom bar */
+static const int user_bh            = 25;       /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
+static const int decorhints			= 1;	   /* 1 means respect decoration hints */
 
-static const char *fonts[]          = { "Hack Nerd Font:size=14:style=Bold" };  /*{ "monospace:size=10" };*/
-static const char dmenufont[]       = "Source Code Pro:size=14:style=Bold"; /*"monospace:size=10";*/
+static const char *fonts[]          = { "Hack Nerd Font:size=11:style=Bold" };  /*{ "monospace:size=10" };*/
+static const char dmenufont[]       = "Source Code Pro:size=11:style=Bold"; /*"monospace:size=10";*/
 
 static const char red[]       = "#f28fad";
 static const char orange[]    = "#f8bd96";
@@ -35,7 +36,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "﯂", "", "", "", "", "", "" };
+static const char *tags[] = { "", "󰍳", "󰇥", "󱢢", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -45,6 +46,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	//{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Eww",	  NULL,       NULL,       0,			1,			 -1 },
 };
 
 /* layout(s) */
@@ -60,7 +62,7 @@ static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen win
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ " ",      tile },    /* first entry is default */
-	{ "ﳝ ",      NULL },    /* no layout function means floating behavior */
+	{ "󰧱",      NULL },    /* no layout function means floating behavior */
 	//{ "[M]",      monocle },
 };
 
@@ -77,16 +79,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", blue, "-sb", blue, "-sf", black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, NULL };
 //static const char *termcmd[]  = { "alacritty", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *browser[]	 = { "chromium", NULL };
+static const char *browser[]	 = { "chromium-browser", NULL };
 static const char *filemanager[] = { "pcmanfm", NULL };
 static const char *wallpaper[]   = { "nitrogen", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	//{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_r,      spawn,		   SHCMD("~/scripts/menu.sh") },
 	{ MODKEY,		                XK_Return, spawn,          {.v = termcmd } },
 	//{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Right,  focusstack,     {.i = +1 } },
@@ -128,7 +131,10 @@ static const Key keys[] = {
 	{0,								XF86XK_AudioRaiseVolume, spawn,		   SHCMD("amixer -q sset Master 5%+") },
 	{0,								XF86XK_AudioMute,		 spawn,		   SHCMD("amixer -q sset Master toggle") },
 	{ MODKEY,						XK_Print,				 spawn,		   SHCMD("flameshot full -p ~/Pictures/screenshots") },
-	{ MODKEY,						XK_t,					 spawn,		   SHCMD("$HOME/dwm/trayer.sh") },
+	{ MODKEY,						XK_t,					 spawn,		   SHCMD("$HOME/dwm-kiefciman/trayer.sh") },
+	{0,								XF86XK_MonBrightnessDown,spawn,		   SHCMD("brightnessctl set 5%-") },	
+	{0,								XF86XK_MonBrightnessUp,  spawn,		   SHCMD("brightnessctl set +5%") },	
+	{ MODKEY,						XK_d,					 spawn,		   SHCMD("~/scripts/dashboard.sh") },
 
 };
 
--- stats.sh
+++ stats.sh
@@ -8,7 +8,8 @@ do
 	volume="$(amixer get Master | sed 6!d | cut -d'[' -f2 | cut -d']' -f1)"
 	mem="$(free -h | sed 2!d | cut -d'i' -f2 | sed 's/^[ \t]*//')"
 	cpu="$(./cpu | sed 1!d)"
-	gpu="$(nvidia-smi | sed 9!d | cut -d' ' -f5)"
-	xsetroot -name " 󰇺 $cpu  $mem  $gpu  $volume  $current_date  $current_time "
+	#gpu="$(nvidia-smi | sed 9!d | cut -d' ' -f5)"
+	#xsetroot -name " 󰇺 $cpu  $mem  $gpu  $volume  $current_date  $current_time "
+	xsetroot -name " 󰇺 $cpu  $mem  $volume  $current_date  $current_time "
 	sleep 1
 done
